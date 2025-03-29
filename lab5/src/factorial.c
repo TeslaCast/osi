f5e3cd63d373d99050338fa74818f06cb8823608
TeslaCast <159522080+TeslaCast@users.noreply.github.com>
2025-03-29 23:54:16 +0000

    Lab5 Готова

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -16,28 +16,25 @@
 #include <pthread.h>
 
 struct Segment {
-	int begin;
-	int end;
-	int mod;
+    int begin;
+    int end;
+    int mod;
 };
 
 int result = 1;
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
-void factorial(void *segments) {		
-	struct Segment *segment = (struct Segment *)segments;
-int temp_result = 1;
-	for (int i = (*segment).begin; i <= (*segment).end; i++)
-	{
-		temp_result *= ((i % (*segment).mod) == 0 ? 1 : i);
-		sleep(1);
-		//printf("current pid:%d \n",(*segment).begin);
-		//printf("%d\n",temp_result);
- 	}
-pthread_mutex_lock(&mut);
-result *= temp_result;
-pthread_mutex_unlock(&mut);
+void* factorial(void* segments) {
+    struct Segment* segment = (struct Segment*)segments;
+    int temp_result = 1;
+    for (int i = segment->begin; i <= segment->end; i++) {
+        temp_result *= ((i % segment->mod) == 0 ? 1 : i);
+    }
+    pthread_mutex_lock(&mut);
+    result *= temp_result;
+    pthread_mutex_unlock(&mut);
+    return NULL;
 }
 
 int main(int argc, char** argv) {
@@ -48,57 +45,43 @@ int main(int argc, char** argv) {
     while (true) {
         int current_optind = optind ? optind : 1;
 
-        static struct option options[] = { {"k", required_argument, 0, 0},
-                                          {"pnum", required_argument, 0, 0},
-                                          {"mod", required_argument, 0, 0},
-                                          {0, 0, 0, 0} };
+        static struct option options[] = {
+            {"k", required_argument, 0, 'k'},
+            {"pnum", required_argument, 0, 'p'},
+            {"mod", required_argument, 0, 'm'},
+            {0, 0, 0, 0}
+        };
 
         int option_index = 0;
-        int c = getopt_long(argc, argv, "f", options, &option_index);
+        int c = getopt_long(argc, argv, "k:p:m:", options, &option_index);
 
         if (c == -1) break;
 
         switch (c) {
-        case 0:
-            switch (option_index) {
-            case 0:
+            case 'k':
                 k = atoi(optarg);
-                if (k <= 0)
-                {
+                if (k <= 0) {
                     printf("k is a positive number\n\n");
                     return 1;
                 }
                 break;
-            case 1:
+            case 'p':
                 pnum = atoi(optarg);
-                if (pnum <= 0)
-                {
+                if (pnum <= 0) {
                     printf("pnum is a positive number\n");
                     return 1;
                 }
                 break;
-            case 2:
+            case 'm':
                 mod = atoi(optarg);
-                if (mod <= 0)
-                {
+                if (mod <= 0) {
                     printf("mod is a positive number\n");
                     return 1;
                 }
                 break;
-
-            defalut:
-                printf("Index %d is out of options\n", option_index);
-            }
-            break;
-
-        case 'f':
-            break;
-
-        case '?':
-            break;
-
-        default:
-            printf("getopt returned character code 0%o?\n", c);
+            default:
+                printf("Usage: %s -k <num> -p <num> -m <num>\n", argv[0]);
+                return 1;
         }
     }
 
@@ -108,33 +91,31 @@ int main(int argc, char** argv) {
     }
 
     if (k == -1 || pnum == -1 || mod == -1) {
-        printf("Usage: %s --k \"num\" --pnum \"num\" --mod \"num\" \n",
-            argv[0]);
+        printf("Usage: %s -k <num> -p <num> -m <num>\n", argv[0]);
         return 1;
     }
 
-pthread_t threads[pnum];
-struct Segment segment[pnum];
-for (int i = 0; i < pnum; i++) 
-	{
-		segment[i].begin = (k/pnum)*i + 1;
-      		segment[i].end = ((i != pnum - 1) ? (k/pnum)*(i+1) : k);
-		segment[i].mod = mod;
-     		if (pthread_create(&threads[i], NULL, (void *)factorial, (void *)(segment+i)) != 0) 
-		{
-      			printf("Error: pthread_create failed!\n");
-      			return 1;
-      		}
-	}
+    pthread_t threads[pnum];
+    struct Segment segment[pnum];
+    int segment_size = k / pnum;
 
-for (int i = 0; i < pnum; i++) 
-{
-	if(pthread_join(threads[i], NULL) != 0)
-{
-    perror("pthread_join");
-    exit(1);
-}
-}
-printf("\nResult: %d\n", result);
+    for (int i = 0; i < pnum; i++) {
+        segment[i].begin = (segment_size * i) + 1;
+        segment[i].end = (i != pnum - 1) ? (segment_size * (i + 1)) : k;
+        segment[i].mod = mod;
+        if (pthread_create(&threads[i], NULL, factorial, (void*)(segment + i)) != 0) {
+            printf("Error: pthread_create failed!\n");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < pnum; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            perror("pthread_join");
+            exit(1);
+        }
+    }
+
+    printf("\nResult: %d\n", result);
     return 0;
 }
