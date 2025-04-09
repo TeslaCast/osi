c88755382e31f9cde3245c2491b7f6f8ce2f55dc
TeslaCast <159522080+TeslaCast@users.noreply.github.com>
2025-04-09 08:24:40 +0000

    Lab7 готова

--- lab7/src/tcpserver.c
+++ lab7/src/tcpserver.c
@@ -2,119 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <stdbool.h>
-#include <getopt.h>
-
-
-//#define SERV_PORT 10050
-//#define BUFSIZE 100
 #define SADDR struct sockaddr
 
-int main(int argc, char **argv) {
-    const size_t kSize = sizeof(struct sockaddr_in);
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    printf("Usage: %s <PORT> <BUFSIZE>\n", argv[0]);
+    exit(1);
+  }
 
-    int lfd, cfd;
-    int nread;
+  int serv_port = atoi(argv[1]);
+  int bufsize = atoi(argv[2]);
+  char *buf = malloc(bufsize);
+  if (buf == NULL) {
+    perror("malloc");
+    exit(1);
+  }
 
+  const size_t kSize = sizeof(struct sockaddr_in);
+  int lfd, cfd, nread;
+  struct sockaddr_in servaddr, cliaddr;
 
-    int SERV_PORT = -1;
-    int BUFSIZE = -1;
-    while (true) {
-        int current_optind = optind ? optind : 1;
+  if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
 
-        static struct option options[] = { {"port", required_argument, 0, 0},
-                                          {"bufsize", required_argument, 0, 0},
-                                          {0, 0, 0, 0} };
+  memset(&servaddr, 0, kSize);
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(serv_port);
 
-        int option_index = 0;
-        int c = getopt_long(argc, argv, "", options, &option_index);
+  if (bind(lfd, (SADDR *)&servaddr, kSize) < 0) {
+    perror("bind");
+    exit(1);
+  }
 
-        if (c == -1)
-            break;
+  if (listen(lfd, 5) < 0) {
+    perror("listen");
+    exit(1);
+  }
 
-        switch (c) {
-        case 0: {
-            switch (option_index) {
-            case 0:
-                SERV_PORT = atoi(optarg);
-                // TODO: your code here
-                if (SERV_PORT <= 0)
-                {
-                    printf("port is a positive number");
-                }
-                break;
-            case 1:
-                BUFSIZE = atoi(optarg);
-                // TODO: your code here
-                if (BUFSIZE < 0)
-                {
-                    printf("bufsize is a positive number");
-                }
-                break;
-            default:
-                printf("Index %d is out of options\n", option_index);
-            }
-        } break;
+  while (1) {
+    unsigned int clilen = kSize;
 
-        case '?':
-            printf("Unknown argument\n");
-            break;
-        default:
-            fprintf(stderr, "getopt returned character code 0%o?\n", c);
-        }
+    if ((cfd = accept(lfd, (SADDR *)&cliaddr, &clilen)) < 0) {
+      perror("accept");
+      exit(1);
+    }
+    printf("Connection established\n");
+
+    while ((nread = read(cfd, buf, bufsize)) > 0) {
+      write(1, buf, nread);
     }
 
-    if (SERV_PORT == -1 || BUFSIZE == -1) {
-        fprintf(stderr, "Using: %s --port 10050 --bufsize 100\n", argv[0]);
-        return 1;
+    if (nread == -1) {
+      perror("read");
+      exit(1);
     }
+    close(cfd);
+  }
 
-    char buf[BUFSIZE];
-    struct sockaddr_in servaddr;
-    struct sockaddr_in cliaddr;
-
-    if ((lfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket");
-        exit(1);
-    }
-
-    memset(&servaddr, 0, kSize);
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);
-
-    if (bind(lfd, (SADDR*)&servaddr, kSize) < 0) {
-        perror("bind");
-        exit(1);
-    }
-
-    if (listen(lfd, 5) < 0) {
-        perror("listen");
-        exit(1);
-    }
-
-    while (1) {
-        unsigned int clilen = kSize;
-
-        if ((cfd = accept(lfd, (SADDR*)&cliaddr, &clilen)) < 0) {
-            perror("accept");
-            exit(1);
-        }
-        printf("connection established\n");
-
-        while ((nread = read(cfd, buf, BUFSIZE)) > 0) {
-            write(1, &buf, nread);
-        }
-
-        if (nread == -1) {
-            perror("read");
-            exit(1);
-        }
-        close(cfd);
-    }
+  free(buf);
 }
